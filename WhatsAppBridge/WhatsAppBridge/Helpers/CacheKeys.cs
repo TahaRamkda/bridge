b2396c415097ec29051d44c819279e6a436e3944
Huzeifa Ambua <huzeifa.a@koutfood.com>
2025-04-04 16:42:23 +0300

    Huzeifa - updated caching

--- WhatsAppBridge/WhatsAppBridge/Helpers/CacheKeys.cs
+++ WhatsAppBridge/WhatsAppBridge/Helpers/CacheKeys.cs
@@ -10,18 +10,8 @@
         /// <summary>
         /// {0} - ClientId
         /// </summary>
-        public static readonly string CLIENTS_BY_ID_KEY = "Bridge.Client.Id-{0}";
-
-        /// <summary>
-        /// </summary>
-        public static readonly string CLIENTS_DROPDOWN_KEY = "Bridge.Client.dropdown";
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public static readonly string SENDERS_DROPDOWN_KEY = "Bridge.Sender.dropdown-{0}";
-
-
+        public static string CLIENTS_BY_ID_KEY = "Bridge.Client.Id-{0}";
+          
         /// <summary>
         /// Key pattern to clear cache
         /// </summary>
@@ -31,6 +21,6 @@
         /// {0} - ClientId 
         /// {1} - SenderId
         /// </summary>
-        public static readonly string SENDERS_BY_CLIENTID_SENDERID_KEY = "Bridge.Sender.{0}-{1}";
+        public static string SENDERS_BY_CLIENTID_SENDERID_KEY = "Bridge.Sender.{0}-{1}";
     }
 }
